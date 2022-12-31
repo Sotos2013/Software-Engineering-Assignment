89ab7124567f26d33ef3fd3c87cbc6f89790e359
Sotos2013 <76269695+Sotos2013@users.noreply.github.com>
2022-12-31 12:05:11 +0200

    Delete Web-App directory

--- Web-App/resources/main_footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="footer mt-auto py-3 bg-dark">
-	<div class="container">
-		<span class="text-muted">Εργασία Εξαμήνου, Ανάπτυξη Εφαρμογών για Κινητές Συσκευές - 6ο Εξάμηνο.</span><br>
-		<span class="text-muted">&copy; Copyright Ομάδα 1, 2021-<?php echo date("Y") ?>.</span>
-	</div>
-</footer>
