fef45344e309fc3b35a826603f75c4911c7f802d
Sotos2013 <www.sotihatzi@gmail.com>
2026-03-11 19:03:30 +0200

    change redirection problem

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
-
 session_start();
 require_once './resources/config.php';
 
-// If the user is logged in, he gets redirected at the administration dashboard.!!!
+// 1. Αν είναι Admin, στείλτον στο admin dashboard
 if(isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] === true) {
-	header('Location: ' . AREF_DIR_ADMIN);
-	die();
+    header('Location: ' . AREF_DIR_ADMIN);
+    exit;
+} 
+// 2. Αν είναι απλός χρήστης, στείλτον στο user dashboard
+elseif(isset($_SESSION["log_in"]) && $_SESSION["log_in"] === true) {
+    header('Location: ' . AREF_DIR_USER);
+    exit;
 }
-else { // Otherwise, he is prompted to login
-	header('Location: ' . AREF_LOGIN);
-	die();
+// 3. Αν ΔΕΝ είναι συνδεδεμένος, στείλτον ΜΟΝΟ στο login
+else {
+    // Χρησιμοποιούμε το πλήρες URL για να μην μπερδευτεί ο Apache
+    header('Location: https://users.iee.ihu.gr/~iee2019187/Software-Engineering-Assignment/public_html/login/');
+    exit;
 }
-
 ?>
