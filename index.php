1119c9ef038baa87d7586de886e92dd7da857b5d
Sotos2013 <www.sotihatzi@gmail.com>
2026-03-11 19:08:17 +0200

    ....

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php
 session_start();
 
-// Προσωρινό check χωρίς config για να δούμε αν φταίνε οι σταθερές
+// Ορίζουμε το βασικό URL του project σου
+$base_url = "https://users.iee.ihu.gr/~iee2019187/Software-Engineering-Assignment/public_html/";
+
 if(isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] === true) {
-    header('Location: ./admin/index.php');
+    header('Location: ' . $base_url . 'admin/index.php');
     exit;
 } 
+elseif(isset($_SESSION["log_in"]) && $_SESSION["log_in"] === true) {
+    header('Location: ' . $base_url . 'user/index.php');
+    exit;
+}
 else {
-    // Στέλνουμε απευθείας στο login με σχετικό path
-    header('Location: ./login/index.php');
+    // Στέλνουμε απευθείας στο login με πλήρες URL
+    header('Location: ' . $base_url . 'login/index.php');
     exit;
 }
 ?>
