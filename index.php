1320e182218991dbcb3367589cb04068b41914b5
Sotos2013 <www.sotihatzi@gmail.com>
2026-03-11 18:28:28 +0200

    nothing

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 session_start();
 require_once './resources/config.php';
 
-// If the user is logged in, he gets redirected at the administration dashboard.
+// If the user is logged in, he gets redirected at the administration dashboard.!!!
 if(isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] === true) {
 	header('Location: ' . AREF_DIR_ADMIN);
 	die();
